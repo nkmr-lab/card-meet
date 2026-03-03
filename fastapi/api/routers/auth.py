e00e35816e0e1324d16b32ce16dbb753a15a552d
non2d <onedanijo11@gmail.com>
2026-03-03 17:00:47 +0900

    環境変数を少し修正

--- fastapi/api/routers/auth.py
+++ fastapi/api/routers/auth.py
@@ -23,7 +23,7 @@ def generate_auth_token():
             "exp": int(time.time()) + 60 * 60 * 24 * 2,  # 72時間より長いとエラー．死んでも7日とかにするな． 
             "scope": {
                 "app": {
-                    "id": os.getenv("APP_ID"),
+                    "id": os.getenv("SKYWAY_APP_ID"),
                     "turn": True,
                     "actions": ["read"],
                     "channels": [
@@ -69,7 +69,7 @@ def generate_admin_auth_token():
         "iat": int(time.time()),  # 現在のUNIXタイムスタンプ
         "jti": str(uuid.uuid4()),  # ランダムなUUID
         "exp": int(time.time()) + 60 * 60 * 24 * 2,  # 1週間後のUNIXタイムスタンプ
-        "appId": os.getenv("APP_ID")  # アプリケーションID
+        "appId": os.getenv("SKYWAY_APP_ID")  # アプリケーションID
     }
 
     SECRET_KEY = os.getenv("SKYWAY_SECRET_KEY")  # シークレットキー
